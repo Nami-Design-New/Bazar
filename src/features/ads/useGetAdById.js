483d2aa9f44f522aa9573b17c986a2129ea4735b
Ahmed Abd El-Ghany <ahmed.abdelghany1211@gmail.com>
2024-09-09 14:44:05 +0300

    profile, auth with login & some updates

---
+++ src/features/ads/useGetAdById.js
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import { getAdById } from "../../services/apiAds";
+import { useParams } from "react-router-dom";
+
+function useGetAdById() {
+  const { id } = useParams();
+
+  const { isLoading, data, error } = useQuery({
+    queryKey: ["adById", id],
+    queryFn: () => getAdById(id),
+    retry: false,
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+    refetchOnReconnect: false,
+  });
+
+  return { isLoading, data, error };
+}
+
+export default useGetAdById;
