4df676f3bbae5fecc1c3ab102e2979f6bf1b2df3
Ahmed Abd El-Ghany <ahmed.abdelghany1211@gmail.com>
2024-09-16 15:34:30 +0300

    intersets

---
+++ src/features/interests/useUserInterests.js
@@ -0,0 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
+import { getUserInterests } from "../../services/apiInterests";
+
+function useUserInterests() {
+  const { isLoading, data, error } = useQuery({
+    queryKey: ["userInterests"],
+    queryFn: getUserInterests,
+    retry: false,
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+    refetchOnReconnect: false,
+
+  });
+
+  return { isLoading, data, error };
+}
+
+export default useUserInterests
